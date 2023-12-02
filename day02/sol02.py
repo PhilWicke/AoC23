fa98bd3d1cfda7fbf8279f88b1ed5b3a4dd784d8
Wicke <pwicke@CISMacPro.local>
2023-12-02 20:44:08 +0100

    solution day02 ex02

--- day02/sol02.py
+++ day02/sol02.py
@@ -1,4 +1,14 @@
-mode = "test"
+mode = "code"
 with open(mode+".txt", "r") as f_in:
-    lines = f_in.readlines()
+    lines = [line.strip() for line in f_in.readlines()]
 
+result = 0
+for idx, game in enumerate(lines):
+    content = game.split(": ")[1]
+    samples = content.replace(", ","; ").split("; ")
+    g_max = max([int(s.split(" ")[0]) for s in samples if s.endswith("n")])
+    b_max = max([int(s.split(" ")[0]) for s in samples if s.endswith("e")])
+    r_max = max([int(s.split(" ")[0]) for s in samples if s.endswith("d")])
+    result += g_max*b_max*r_max
+
+print(result)
