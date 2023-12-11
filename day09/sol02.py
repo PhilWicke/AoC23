a402ca24a8e83b461072f934352562534d642d81
Wicke <pwicke@CISMacPro.local>
2023-12-11 14:31:49 +0100

    day11 ex01

--- day09/sol02.py
+++ day09/sol02.py
@@ -1,3 +1,24 @@
-mode = "test2"
+mode = "code"
 with open(mode+".txt", "r") as f_in:
-    lines = [line.strip() for line in f_in.readlines()]
+    lines = f_in.readlines()
+
+data = [line.strip().split() for line in lines]
+
+def get_dif_seq(seq):
+    return [seq[i+1]-seq[i] for i in range(len(seq)-1)]
+
+result = 0
+for seq in data:
+    seq.reverse()
+    seq = [int(x) for x in seq]
+    lasts = [seq[-1]]
+    deriv_seq = seq
+    while deriv_seq != [0]*len(deriv_seq):
+        #print(deriv_seq)
+        deriv_seq = get_dif_seq(deriv_seq)
+        lasts.append(deriv_seq[-1])
+        
+    result+= sum(lasts)
+    #print()
+
+print(result)
--- day11/sol01.py
+++ day11/sol01.py
@@ -1,5 +1,28 @@
-mode = "test"
+mode = "code"
 with open(mode+".txt", "r") as f_in:
-    lines = f_in.readlines()
+    data = [line.strip() for line in f_in.readlines()]
 
-print(lines)
+def distance(t1, t2):
+    return abs(t1[0]-t2[0])+abs(t1[1]-t2[1])
+
+universeA = [] # expand columns
+for row in data:
+    universeA.append(row)
+    if "#" not in row:
+        universeA.append(row)
+
+universeB = [] # transpose to expand rows
+for column in range(len(universeA[0])):
+    col = "".join([universeA[i][column] for i in range(len(universeA))])
+    universeB.append(col)
+    if "#" not in col:
+        universeB.append(col)
+
+# transpose again
+universe = [ "".join([universeB[i][column] for i in range(len(universeB))]) for column in range(len(universeB[0]))]
+# collect galaxies
+galaxies = [(x,y) for y,line in enumerate(universe) for x, val in enumerate(line) if val == "#"]
+# sum up manhattan distances
+result = sum([distance(g, galaxies[j]) for i,g in enumerate(galaxies) for j in range(i+1,len(galaxies))])
+
+print(result)
